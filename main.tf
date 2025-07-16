ed3869cfa675313167b3c9d3b46daf7f77cfbc19
dbamfo <bamfodaniel@yahoo.com>
2025-07-16 23:35:02 +0100

    changed bucket region buildspec and backend

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_ecr_repository" "foo" {
   image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
 }
